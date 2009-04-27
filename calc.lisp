0e01ec9373359655a712c8b9aa6107b57495b404
Dmitry Statyvka <dstatyvka@gmail.com>
2009-04-27 22:22:17 +0300

    Removed extra-spaces at ends of lines

--- calc.lisp
+++ calc.lisp
@@ -8,9 +8,9 @@
 	   (x-var (variance xs x-mean))
 	   (y-var (variance ys y-mean))
 	   (xy-v (covariance xs ys x-mean y-mean))
-	   (m (make-marray 'double-float 
+	   (m (make-marray 'double-float
 			   :initial-contents `((,x-var ,xy-v)
 					       (,xy-v  ,y-var))))
-	   (ev (multiple-value-list 
+	   (ev (multiple-value-list
 		(eigenvalues-eigenvectors m))))
       (values x-mean y-mean x-var y-var xy-v ev))))
